7551a7abab232d496af74dac4f1bcc59d74b76e4
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-04-12 16:51:24 +0200

    gradle build

---
+++ TheJacketProject/app/src/main/java/thejacketproject/Controller/MainSceneController.java
@@ -0,0 +1,39 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class MainSceneController {
+
+    @FXML
+    private Pane buttonPane;
+
+    @FXML
+    private Button closetButton;
+
+    @FXML
+    private AnchorPane mainAnchorPane;
+
+    @FXML
+    private Pane standInPane;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    void displayCloset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showProfile(ActionEvent event) {
+
+    }
+
+}
