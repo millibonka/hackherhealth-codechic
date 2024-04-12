9d14d57d8b5cf7a03d6836dadc828bdde029c159
Chelsea Alambuya <64859402+alambuyachelsea@users.noreply.github.com>
2024-04-12 23:35:33 +0200

    javafx gui

--- TheJacketProject/app/src/main/java/thejacketproject/Controller/AppLaunchController.java
+++ TheJacketProject/app/src/main/java/thejacketproject/Controller/AppLaunchController.java
@@ -1,15 +1,41 @@
+package thejacketproject.Controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+/**
+ * Opens the welcome scen to the homw scene.
+ */
 public class AppLaunchController {
 
-    @FXML
-    private Button getDressed;
+  private Stage primaryStage;
 
-    @FXML
-    void openHome(ActionEvent event) {
+  public void setPrimaryStage(Stage primaryStage) {
+    this.primaryStage = primaryStage;
+  }
 
-    }
+  @FXML
+  private Button getDressed;
+
+  @FXML
+  void openHome(ActionEvent event) throws Exception  {
+
+    // Load the FXML file
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainScene.fxml"));
+    Parent root = loader.load();
+
+    // Get the controller instance
+    MainSceneController controller = loader.getController();
+    controller.setPrimaryStage(primaryStage);
+
+    Scene newScene = new Scene(root);
+    primaryStage.setScene(newScene);
+
+  }
 
 }
--- TheJacketProject/app/src/main/java/thejacketproject/Controller/MainSceneController.java
+++ TheJacketProject/app/src/main/java/thejacketproject/Controller/MainSceneController.java
@@ -1,39 +1,50 @@
+package thejacketproject.Controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+/**
+ * The main page controller.
+ */
 public class MainSceneController {
+  private Stage primaryStage;
 
-    @FXML
-    private Pane buttonPane;
+  public void setPrimaryStage(Stage primaryStage) {
+    this.primaryStage = primaryStage;
+  }
 
-    @FXML
-    private Button closetButton;
+  @FXML
+  private Pane buttonPane;
 
-    @FXML
-    private AnchorPane mainAnchorPane;
+  @FXML
+  private Button closetButton;
 
-    @FXML
-    private Pane standInPane;
+  @FXML
+  private AnchorPane mainAnchorPane;
 
-    @FXML
-    private Button userButton;
+  @FXML
+  private Pane standInPane;
 
-    @FXML
-    void displayCloset(ActionEvent event) {
+  @FXML
+  private Button userButton;
 
-    }
+  @FXML
+  void displayCloset(ActionEvent event) {
 
-    @FXML
-    void goHome(ActionEvent event) {
+  }
 
-    }
+  @FXML
+  void goHome(ActionEvent event) {
 
-    @FXML
-    void showProfile(ActionEvent event) {
+  }
 
-    }
+  @FXML
+  void showProfile(ActionEvent event) {
+
+  }
 
 }
